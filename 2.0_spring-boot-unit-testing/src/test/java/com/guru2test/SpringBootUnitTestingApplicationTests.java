aa0617388785331d823db80acd3b87c357e93156
meher12 <meher.khemiri@gmail.com>
2023-06-05 12:25:04 +0100

    Spring boot Unit Testing:: assertEquals and assertNotEquals

--- 2.0_spring-boot-unit-testing/src/test/java/com/guru2test/SpringBootUnitTestingApplicationTests.java
+++ 2.0_spring-boot-unit-testing/src/test/java/com/guru2test/SpringBootUnitTestingApplicationTests.java
@@ -3,6 +3,7 @@ package com.guru2test;
 import com.guru2test.models.CollegeStudent;
 import com.guru2test.models.StudentGrades;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class SpringBootUnitTestingApplicationTests {
@@ -49,6 +53,29 @@ class SpringBootUnitTestingApplicationTests {
 		studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.0, 91.75)));
 		student.setStudentGrades(studentGrades);
 	}
+	@DisplayName("Add grade results for student grades equals")
+	@Test
+	public void addGradeResultsForStudentGradesAssertEquals(){
+		assertEquals(352.75, studentGrades.addGradeResultsForSingleClass(
+				student.getStudentGrades().getMathGradeResults()
+		));
+	}
+
+	@DisplayName("Add grade results for student grades not equals")
+	@Test
+	public void addGradeResultsForStudentGradesAssertNotEquals(){
+		assertNotEquals(0, studentGrades.addGradeResultsForSingleClass(
+				student.getStudentGrades().getMathGradeResults()
+		));
+	}
+
+
+
+
+
+
+
+
 	@Test
 	void basicTest(){
 
--- README.md
+++ README.md
@@ -119,4 +119,5 @@
 ## 3- Spring Boot Unit Testing Support:
 1. Create project `2.0_spring-boot-unit-testing` to see all dependency tree `mvn dependency:tree`
    * Must be the project has the same package name in `java package` and `test package`
-   * Read application.properties and Inject Spring Beans
+2. Spring boot Unit Testing:: Read `application.properties` and Inject Spring Beans
+3. Spring boot Unit Testing:: assertEquals and assertNotEquals
